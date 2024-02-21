1ecbc15c95278ece4a6c5223a841558395b9d082
Shane Howe <shanehowe100@hotmail.com>
2024-02-21 22:07:21 +0000

    Add member listing functionality

--- public/views/members/view.php
+++ public/views/members/view.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/styles/styles.css">
     <title>View Members</title>
+    <?php require_once "../../../envParser.php" ?>
+
+    <?php
+    function getMembers()
+    {
+        $db_username = $_ENV["DB_USERNAME"];
+        $db_host = $_ENV["DB_HOST"];
+        $db_name = $_ENV["DB_NAME"];
+
+        $pdo = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name, $db_username, '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $result = $pdo->query("SELECT * FROM members");
+        while ($row = $result->fetch()) {
+            echo "<tr>";
+            echo "<td>" . $row["memberid"] . "</td>";
+            echo "<td>" . $row["FirstName"] . "</td>";
+            echo "<td>" . $row["LastName"] . "</td>";
+            echo "<td>" . $row["Status"] . "</td>";
+            echo "</tr>";
+        }
+    }
+    ?>
 </head>
+
 <body>
     <?php include "../../includes/navbar.php" ?>
 
@@ -16,12 +41,29 @@
                     <div>
                         <label for="searchbar">Name</label>
                     </div>
-                    <input type="text" name="memberSearch" id="searchbar" placeholder="Enter members name..."/>
+                    <input type="text" name="memberSearch" id="searchbar" placeholder="Enter members name..." />
                     <input type="button" value="Search" type="submit" id="search-button-input" />
                 </form>
             </div>
+
+            <section class="members-table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php getMembers(); ?>
+                    </tbody>
+                </table>
+            </section>
         </main>
         <?php include "../../includes/membersSidebar.php" ?>
     </div>
 </body>
+
 </html>
