bd60057915628a9490d996a4989b064146b5bbe7
Shane Howe <shanehowe100@hotmail.com>
2024-02-22 12:53:57 +0000

    Add new member view and include necessary files

---
+++ public/views/members/add.php
@@ -0,0 +1,9 @@
+<?php
+
+$includeDirectory = "../../includes";
+include "../../includes/header.html";
+include "../../includes/navbar.php";
+include $includeDirectory . "/addMemberForm.html";
+include $includeDirectory . "/membersSidebar.php";
+
+include $includeDirectory . "/footer.html";
