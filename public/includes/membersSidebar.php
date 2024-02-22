1809e4ae058d666a07c5ffc559f7b0c85848d796
Shane Howe <shanehowe100@hotmail.com>
2024-02-22 12:53:38 +0000

    add hrefs for routing to pages

--- public/includes/membersSidebar.php
+++ public/includes/membersSidebar.php
@@ -1,12 +1,14 @@
+<?php include_once "../../../config.php"; ?>
+
 <aside class="sidebar">
     <div class="sidebar-item-heading">
         <h3>Quick Links</h3>
     </div>
     <div class="sidebar-item">
-        View members
+        <a href="<?php echo BASE_URL;?>views/members/view.php">View members</a>
     </div>
     <div class="sidebar-item">
-        Add member
+        <a href="<?php echo BASE_URL;?>views/members/add.php">Add member</a>
     </div>
     <div class="sidebar-item">
         Edit member
