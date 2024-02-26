fade13d8540edae711ef7519b6cdba213e0cc4f4
Shane Howe <shanehowe100@hotmail.com>
2024-02-26 19:47:12 +0000

    Refactor member addition functionality

--- views/members/add.php
+++ views/members/add.php
@@ -1,20 +1,21 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/utils/database.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/services/member_service.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/services/membership_type_service.php';
 
 $firstName = "";
 $lastName = "";
 $email = "";
 $phone = "";
-
+$membershipType = "";
 $formError = "";
 
-if (isset($_POST['submit-member'])) {
-    $pdo = getDatabaseConnection();
+if (isset($_POST['submitMember'])) {
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
+    $membershipType = $_POST['membershipType'];
     $startDate = date("Y-m-d");
 
     if (strlen($firstName) === 0) {
@@ -25,14 +26,24 @@ if (isset($_POST['submit-member'])) {
         $formError = "Email is required";
     } else if (strlen($phone) === 0) {
         $formError = "Phone is required";
+    } else if (strlen($membershipType) === 0) {
+        $formError = "Membership type is required";
+    } else {
+        try {
+            insertMember($firstName, $lastName, $email, $phone, $membershipType);
+            header("Location: /views/members/view.php");
+            $formError = "";
+        } catch (Exception $e) {
+            $formError = "An error occurred while adding the member";
+        }
     }
-    // Else do database stuff. not ready yet..
+   
 }
 
 $root = $_SERVER['DOCUMENT_ROOT'];
 include $root . "/includes/header.html";
 include $root . "/includes/navbar.php";
-include $root . "/includes/addMemberForm.html";
+include $root . "/includes/addMemberForm.php";
 include $root . "/includes/membersSidebar.php";
 
 include $root . "/includes/footer.html";
