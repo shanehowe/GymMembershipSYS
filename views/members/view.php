699d793f81ca7329915fd46676ff5b72b443c4b3
Shane Howe <shanehowe100@hotmail.com>
2024-02-28 18:02:33 +0000

    Refactor member view page and add member search functionality

--- views/members/view.php
+++ views/members/view.php
@@ -1,50 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . "/services/member_service.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/includes/header.html";
+include $_SERVER['DOCUMENT_ROOT'] . "/includes/navbar.php";
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../assets/styles/styles.css">
-    <title>View Members</title>
+$members = getMembers();
 
-    <?php
-    include_once $_SERVER['DOCUMENT_ROOT'] . "/services/member_service.php"
+if (isset($_POST['memberSearchSubmit'])) {
+    $memberSearch = $_POST['memberSearch'];
+    if (!empty($memberSearch))
+        $members = getMembersByName($_POST['memberSearch']);
+}
+?>
+<div class="main-content-container">
+    <main class="main-content">
+        <div class="searchbar-container">
+            <form action="view.php" method="post">
+                <div>
+                    <label for="searchbar">Search Members</label>
+                </div>
+                <input type="text" name="memberSearch" id="searchbar" placeholder="Enter members name..." />
+                <input value="Search" type="submit" id="search-button-input" name="memberSearchSubmit" />
+            </form>
+        </div>
+
+        <section class="members-table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($members as $member): ?>
+                    <tr class="member-table-row">
+                        <td><?php echo $member['memberid']; ?></td>
+                        <td><?php echo $member['FirstName']; ?></td>
+                        <td><?php echo $member['LastName']; ?></td>
+                        <td><?php echo $member['Status']; ?></td>
+                        <td>Edit</td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </section>
+    </main>
+    <?php 
+    include $_SERVER['DOCUMENT_ROOT'] . "/includes/membersSidebar.php";
+    include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.html";
     ?>
-</head>
-
-<body>
-    <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/navbar.php"; ?>
-
-    <div class="main-content-container">
-        <main class="main-content">
-            <div class="searchbar-container">
-                <form action="">
-                    <div>
-                        <label for="searchbar">Search Members</label>
-                    </div>
-                    <input type="text" name="memberSearch" id="searchbar" placeholder="Enter members name..." />
-                    <input type="button" value="Search" type="submit" id="search-button-input" />
-                </form>
-            </div>
-
-            <section class="members-table-container">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php getMembers(); ?>
-                    </tbody>
-                </table>
-            </section>
-        </main>
-        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/membersSidebar.php" ?>
-    </div>
-</body>
-
-</html>
