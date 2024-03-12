c8d136fe70974fc5436df70069f74f523b680962
Shane Howe <shanehowe100@hotmail.com>
2024-03-12 19:08:57 +0000

    Add nullish coalescing operator and display notification message in member view page

--- views/members/view.php
+++ views/members/view.php
@@ -5,6 +5,9 @@ include $_SERVER['DOCUMENT_ROOT'] . "/includes/navbar.php";
 include_once $_SERVER['DOCUMENT_ROOT'] . "/config.php";
 
 $members = getMembers();
+// Nullish coalescing operator
+$message = $_GET['message'] ?? "";
+$isError = $_GET['isError'] ?? false;
 
 if (isset($_POST['memberSearchSubmit'])) {
     $memberSearch = $_POST['memberSearch'];
@@ -14,6 +17,13 @@ if (isset($_POST['memberSearchSubmit'])) {
 ?>
 <div class="main-content-container">
     <main class="main-content">
+        <?php if (strlen($message) > 0): ?>
+            <div class="<?php echo $isError ? "form-error-container" : "form-success-container" ?> notification-container">
+                <p>
+                    <?php echo $message; ?>
+                </p>
+            </div>
+        <?php endif; ?>
         <div class="searchbar-container">
             <form action="view.php" method="post">
                 <div>
@@ -36,26 +46,35 @@ if (isset($_POST['memberSearchSubmit'])) {
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach($members as $member): ?>
-                    <tr
-                        class="member-table-row" 
-                        data-href="<?php echo BASE_URL . '/views/members/view_member.php?id=' . $member['memberid']; ?>"
-                    >
-                        <td><?php echo $member['memberid']; ?></td>
-                        <td><?php echo $member['FirstName']; ?></td>
-                        <td><?php echo $member['LastName']; ?></td>
-                        <td><?php echo $member['Status']; ?></td>
-                        <td class="mem-action-buttons">
-                            <a class="link-btn edit-btn" href="<?php echo BASE_URL;?>views/members/edit.php?id=<?php echo $member['memberid']; ?>">Edit</a>
-                            <a class="link-btn danger-btn" href="<?php echo BASE_URL;?>views/members/delete.php?id=<?php echo $member['memberid']; ?>">Delete</a>
-                        </td>
-                    </tr>
+                    <?php foreach ($members as $member): ?>
+                        <tr class="member-table-row"
+                            data-href="<?php echo BASE_URL . '/views/members/view_member.php?id=' . $member['memberid']; ?>">
+                            <td>
+                                <?php echo $member['memberid']; ?>
+                            </td>
+                            <td>
+                                <?php echo $member['FirstName']; ?>
+                            </td>
+                            <td>
+                                <?php echo $member['LastName']; ?>
+                            </td>
+                            <td>
+                                <?php echo $member['Status']; ?>
+                            </td>
+                            <td class="mem-action-buttons">
+                                <a class="link-btn edit-btn"
+                                    href="<?php echo BASE_URL; ?>views/members/edit.php?id=<?php echo $member['memberid']; ?>">Edit</a>
+                                <a data-member-id="<?php echo $member['memberid']; ?>"
+                                    class="link-btn danger-btn"
+                                    data-href="<?php echo BASE_URL; ?>views/members/delete.php?id=<?php echo $member['memberid']; ?>">Delete</a>
+                            </td>
+                        </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
         </section>
     </main>
-    <?php 
+    <?php
     include $_SERVER['DOCUMENT_ROOT'] . "/includes/membersSidebar.php";
     include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.html";
     ?>
