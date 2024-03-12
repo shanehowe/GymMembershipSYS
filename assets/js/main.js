a6100b9a9eb5d075678e77863271b9a30130c0c6
Shane Howe <shanehowe100@hotmail.com>
2024-03-12 19:08:31 +0000

    Add delete functionality to member rows

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
 const memberRows = document.querySelectorAll('.member-table-row');
+const deleteMemberLinks = document.querySelectorAll(".danger-btn");
 
 memberRows.forEach((row) => {
     row.addEventListener("dblclick", () => {
         window.location.href = row.getAttribute('data-href');
     });
 })
+
+deleteMemberLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        const dataMemberId = link.getAttribute('data-member-id');
+        if (window.confirm(`Are you sure you want to delete member with ID ${dataMemberId}?`)) {
+            window.location.href = link.getAttribute('data-href');
+        }
+    })
+});
