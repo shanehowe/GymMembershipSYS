58d02d1ff67a7ee36880b40fde40fcf69d12e05e
Shane Howe <shanehowe100@hotmail.com>
2024-03-21 21:06:05 +0000

    Refactor addMemberForm.php to improve form styling and structure

--- includes/addMemberForm.php
+++ includes/addMemberForm.php
@@ -2,69 +2,47 @@
     <main class="main-content">
         <?php if (strlen($formError) > 0): ?>
             <div class="form-error-container notification-container">
-                <p><?php echo $formError; ?></p>
+                <p>
+                    <?php echo $formError; ?>
+                </p>
             </div>
         <?php endif; ?>
 
         <?php if (strlen($formSuccess) > 0): ?>
             <div class="form-success-container notification-container">
-                <p><?php echo $formSuccess; ?></p>
+                <p>
+                    <?php echo $formSuccess; ?>
+                </p>
             </div>
         <?php endif; ?>
         <h3>Add Member</h3>
-        <form action="add.php" method="post" id="add-member-form">
-            <div class="input-container">
-                <div class="input-inside-container">
-                    <div>
-                        <label for="first-name-input">First Name</label>
-                    </div>
-                    <input class="text-input" type="text" name="firstName" id="first-name-input"
-                        placeholder="First name..." value="<?php echo $firstName; ?>">
-                </div>
+        <form action="add.php" method="post" id="add-member-form" class="form-style">
+            <label class="form-label" for="first-name-input">First Name</label>
+            <input class="input-text" type="text" name="firstName" id="first-name-input" placeholder="First name..."
+                value="<?php echo $firstName; ?>">
 
-                <div class="input-inside-container">
-                    <div>
-                        <label for="last-name-input">Last Name</label>
-                    </div>
-                    <input class="text-input" type="text" name="lastName" id="last-name-input"
-                        placeholder="Last name..." value="<?php echo $lastName; ?>">
-                </div>
+            <label class="form-label" for="last-name-input">Last Name</label>
+            <input class="input-text" type="text" name="lastName" id="last-name-input" placeholder="Last name..."
+                value="<?php echo $lastName; ?>">
 
-                <div class="input-inside-container">
-                    <div>
-                        <label for="email-input">Email</label>
-                    </div>
-                    <input class="text-input" type="email" name="email" id="email-input" placeholder="Email..."
-                        value="<?php echo $email; ?>">
-                </div>
+            <label class="form-label" for="email-input">Email</label>
+            <input class="input-text" type="email" name="email" id="email-input" placeholder="Email..."
+                value="<?php echo $email; ?>">
 
-                <div class="input-inside-container">
-                    <div>
-                        <label for="phone-input">Phone</label>
-                    </div>
-                    <input class="text-input" type="text" name="phone" id="phone-input" placeholder="Phone number..."
-                        value="<?php echo $phone; ?>">
-                </div>
+            <label class="form-label" for="phone-input">Phone</label>
+            <input class="input-text" type="text" name="phone" id="phone-input" placeholder="Phone number..."
+                value="<?php echo $phone; ?>">
 
-                <div class="input-inside-container">
-                    <div>
-                        <label for="membership-type-select">Membership Type</label>
-                    </div>
-                    <select class="text-input" name="membershipType" id="membership-type-select" value="<?php echo $membershipType; ?>">
-                        <option value="">Select a membership type...</option>
-                        <?php getActiveMembershipTypes(); ?>
-                    </select>
-                </div>
-            </div>
+            <label class="form-label" for="membership-type-select">Membership Type</label>
+            <select class="input-text" name="membershipType" id="membership-type-select">
+                <option value="">Select a membership type...</option>
+                <?php getActiveMembershipTypes(); ?>
+            </select>
 
             <div class="action-button-container">
-                <div>
-                    <button class="action-button" type="submit" name="submitMember">Add Member</button>
-                </div>
-
-                <div>
-                    <button class="action-button" type="reset">Clear</button>
-                </div>
+                <button class="submit-button" type="submit" name="submitMember">Add Member</button>
+                <button class="submit-button" type="reset">Clear</button>
             </div>
         </form>
+
     </main>
