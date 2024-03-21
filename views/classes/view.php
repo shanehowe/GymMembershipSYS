9bcb0584b72afa8a92dfd564500c271a0867be41
Shane Howe <shanehowe100@hotmail.com>
2024-03-21 21:06:21 +0000

    Add view.php file with required includes and page structure

---
+++ views/classes/view.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/services/member_service.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/services/membership_type_service.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/utils/validation.php';
+
+include $_SERVER['DOCUMENT_ROOT'] . "/includes/header.html";
+include $_SERVER['DOCUMENT_ROOT'] . "/includes/navbar.php";
+
+include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.html";
