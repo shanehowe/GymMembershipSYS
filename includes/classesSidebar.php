6d9e12f2d62c2646a8dda3155f7982eebc09c1b2
Shane Howe <shanehowe100@hotmail.com>
2024-03-21 21:06:09 +0000

    Add classes sidebar to include file

---
+++ includes/classesSidebar.php
@@ -0,0 +1,13 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT']. "/config.php"; ?>
+
+<aside class="sidebar">
+    <div class="sidebar-item-heading">
+        <h3>Quick Links</h3>
+    </div>
+    <div class="sidebar-item">
+        <a href="<?php echo BASE_URL;?>views/classes/view.php">View classes</a>
+    </div>
+    <div class="sidebar-item">
+        <a href="<?php echo BASE_URL;?>views/classes/add.php">Add class</a>
+    </div>
+</aside>
