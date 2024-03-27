755a10cbb0ca0ecd4a8f40ed229409ceee4f8447
Shane Howe <shanehowe100@hotmail.com>
2024-03-27 17:33:01 +0000

    Add new functions for gym class management

--- services/gym_class_service.php
+++ services/gym_class_service.php
@@ -34,3 +34,40 @@ function addGymClass($name, $start_date, $duration, $price, $capacity, $time)
     $stmt->bindParam(':time', $time);
     $stmt->execute();
 }
+
+function getGymClassesThatCanBeBookedAndAreStillActive()
+{
+    $allGymClasses = getGymClasses();
+
+    $gymClassesThatCanBeBookedAndAreStillActive = [];
+    foreach ($allGymClasses as $gymClass) {
+        $classDuration = $gymClass["duration"];
+        $classStartDate = $gymClass["start_date"];
+
+        $dateEndDate = date('Y-m-d', strtotime($classStartDate . ' + ' . $classDuration . ' weeks'));
+        $todaysDate = date('Y-m-d');
+
+        if ($todaysDate < $dateEndDate) {
+            array_push($gymClassesThatCanBeBookedAndAreStillActive, $gymClass);
+        }
+    }
+
+    return $gymClassesThatCanBeBookedAndAreStillActive;
+}
+
+function getBookingCountForGymClass($gymClassId)
+{
+    $pdo = getDatabaseConnection();
+    $stmt = $pdo->prepare("SELECT COUNT(*) FROM bookings WHERE gym_class_id = :gym_class_id");
+    $stmt->bindParam(':gym_class_id', $gymClassId);
+    $stmt->execute();
+    return $stmt->fetchColumn();
+}
+
+function bookMemberIntoGymClass($gymClassId, $memberId)
+{
+    $pdo = getDatabaseConnection();
+    $stmt = $pdo->prepare("INSERT INTO bookings (classID, memberid) VALUES (:classID, :memberid)");
+    $stmt->bindParam(':classID', $gymClassId);
+    $stmt->bindParam(':memberid', $memberId);
+}
